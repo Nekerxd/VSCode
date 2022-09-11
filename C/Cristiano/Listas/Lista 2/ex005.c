3605bd22b941415eff4892894053ae09855e74f9
Nekerxd <nuvielxd_01@hotmail.com>
2022-09-11 17:41:46 -0300

    Lista 2 - Correção ex005.c

--- C/Cristiano/Listas/Lista 2/ex005.c
+++ C/Cristiano/Listas/Lista 2/ex005.c
@@ -8,21 +8,29 @@
 
 int main(){
     setlocale(LC_ALL, "");
-    int idade = 0, maiorid = 0, contador = 0;
+    int idade = 0, maiorid = 0, contador = 0, cabelo, olhos;
     char sexo;
 
     while (idade >-1)
     {
         printf("\nIdade: ");
         scanf("%d", &idade);
+        
+        if (idade != -1){
+            printf("\nSexo [M/F]: ");
+            scanf(" %c", &sexo);
 
-        printf("\nSexo [M/F]: ");
-        scanf(" %c", &sexo);
+            printf("\nCor dos cabelos: \n[0] Castanho \n[1] Preto \n[2] Ruivo \n[3] Louro \nDigite a opção correspondente: ");
+            scanf("%d", &cabelo);
 
-        maiorid = (idade > maiorid ? idade : maiorid);
-        if ((sexo == 'f' || sexo == 'F') && (idade > 17 && idade < 36))
-            contador++;
+            printf("\nCor dos olhos: \n[0] Azuis \n[1] Castanhos \n[2] Verdes \n[3] Avelã \nDigite a opção correspondente: ");
+            scanf("%d", &olhos);
+
+            maiorid = (idade > maiorid ? idade : maiorid);
+            if ((sexo == 'f' || sexo == 'F') && (idade > 17 && idade < 36) && (cabelo == 3) && (olhos == 2))
+                contador++;
+        }
     }
     printf("\nA pessoa mais velha dentre os habitantes tem %d anos.", maiorid);
-    printf("\nExistem %d mulheres com idade entre 18 e 35 anos.", contador);
+    printf("\nDentre os habitantes existem %d mulheres com idade entre 18 e 35 anos, olhos verdes e cabelos louros.", contador);
 }
