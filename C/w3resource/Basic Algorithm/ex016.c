97ebf666d7561da66b23fba5d24a73a2c8d8d2ce
Nekerxd <nuvielxd_01@hotmail.com>
2022-09-26 21:55:56 -0300

    w3resouce exercise fix

--- C/w3resource/Basic Algorithm/ex016.c
+++ C/w3resource/Basic Algorithm/ex016.c
@@ -3,10 +3,11 @@
 #include <stdio.h>
 #include <locale.h>
 #include <time.h>
+#define arrsize 10
 
 int main(){
     setlocale(LC_ALL, "");
-    int arrsize = 10, arr[arrsize], i;
+    int arr[arrsize], i;
     time_t t;
     srand((unsigned) time(&t));
 
@@ -17,11 +18,11 @@ int main(){
 
     arr[5] = arr[4] = 3;
 
-    printf("%d", check(arr, arrsize));
+    printf("%d", check(arr));
 }
 
-int check(int array[], int t_array){
-    for (int i = 0; i < t_array; i++){
+int check(int array[]){
+    for (int i = 0; i < arrsize; i++){
         if (array[i] == array[i+1] && array[i+1] == array[i+2])
             return 1;
     }
